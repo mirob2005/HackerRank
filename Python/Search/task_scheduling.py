8e77d415cad71b35ccbb2f407be32ad3542e319b
Michael Robertson <mirob2005@gmail.com>
2013-05-03 01:06:36 -0700

    Task scheduling work done, should be correct, hackerrank giving all solutions 0/10 even when reusing code that got 6/10

--- Python/Search/task_scheduling.py
+++ Python/Search/task_scheduling.py
@@ -1,27 +1,60 @@
-def addJob(array, deadline,minutes,late):
-    length = len(array)
+def returnIndex(array,number):
+    if not array:
+        return None
+    if len(array) == 1:
+        if number > array[0]:
+            return 0
+        else:
+            return None
+
+    si = 0
+    ei = len(array)-1
+    return binarySearch(array,number,si,ei)
+
+def binarySearch(array,number,si,ei):
+    if si==ei:
+        if number >= array[si]:
+            return si
+        else:
+            return si-1
+    else:
+        middle = (ei-si)//2 +si
+        if number > array[middle]:
+            return binarySearch(array,number,middle+1,ei)
+        elif number < array[middle]:
+            return binarySearch(array,number,si,middle)
+        else:
+            return middle
+
+def addJob(length, array, deadline,minutes,late):
     if length < deadline:
         for i in range(deadline-length):
-            array.append(-1)
+            array.append(i+length)
+        length = deadline
     minLeft = minutes
-    for i in range(1,deadline+1):
-        if array[deadline-i] >= 0:
-            continue
-        else:
-            array[deadline-i] += 1
+    index = returnIndex(array,deadline-1)
+    if index != None:
+        while index >=0 and minLeft >0:
+            array.pop(index)
+            index -= 1
             minLeft -=1
-            if minLeft ==0:
-                break
+        
+    while minLeft >0 and array and array[0] < deadline:
+        array.pop(0)
+        minLeft -=1
     late += minLeft
-    return late
+    return late,length
 
 if __name__ == '__main__':
 
     n = int(input().strip())
     
+    time = 0
+    length = 0
+    
     nl = []
     late = 0
     for op in range(n):
         job = input().split(' ')
-        late = addJob(nl,int(job[0]),int(job[1]),late)
+        late,length = addJob(length,nl,int(job[0]),int(job[1]),late)
         print(late)

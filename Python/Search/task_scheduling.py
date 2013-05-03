9665a44ff5348ba33b24e89a7a2d6211470dc4b4
Michael Robertson <mirob2005@gmail.com>
2013-05-02 17:21:23 -0700

    Latest Task Scheduling at 5/10 (time limit)

--- Python/Search/task_scheduling.py
+++ Python/Search/task_scheduling.py
@@ -1,59 +1,27 @@
-def addJob(array,job):
-    if not array:
-        array.append([job[0],[job[1]]])
-        return
-    if len(array) == 1:
-        if job[0] > array[0][0]:
-            array.append([job[0],[job[1]]])
-        elif job[0] < array[0][0]:
-            array.insert(0,[job[0],[job[1]]])
+def addJob(array, deadline,minutes,late):
+    length = len(array)
+    if length < deadline:
+        for i in range(deadline-length):
+            array.append(-1)
+    minLeft = minutes
+    for i in range(1,deadline+1):
+        if array[deadline-i] >= 0:
+            continue
         else:
-            array[0][1].append(job[1])
-        return
-    si = 0
-    ei = len(array)-1
-    binarySearchInsert(array,job,si,ei)
-    
-def binarySearchInsert(array,job,si,ei):
-    if si==ei:
-        if job[0] > array[si][0]:
-            array.insert(si+1,[job[0],[job[1]]])
-        elif job[0] < array[si][0]:
-            array.insert(si,[job[0],[job[1]]])
-        else:
-            array[si][1].append(job[1])
-        return
-    else:
-        middle = (ei-si)//2 +si
-        
-        if job[0] > array[middle][0]:
-            binarySearchInsert(array,job,middle+1,ei)
-        elif job[0] < array[middle][0]:
-            binarySearchInsert(array,job,si,middle)
-        else:
-            array[middle][1].append(job[1])
-            return
-
-def computeLateness(jobs):
-    maxLate = 0
-    time = 0
-    for deadline in jobs:
-        for job in deadline[1]:
-            time += job
-            if time > deadline[0]:
-                late = (time-deadline[0])
-                if late > maxLate:
-                    maxLate = late
-    print(maxLate)
+            array[deadline-i] += 1
+            minLeft -=1
+            if minLeft ==0:
+                break
+    late += minLeft
+    return late
 
 if __name__ == '__main__':
 
     n = int(input().strip())
     
     nl = []
-    
+    late = 0
     for op in range(n):
         job = input().split(' ')
-        addJob(nl,[int(x) for x in job])
-
-        computeLateness(nl)
+        late = addJob(nl,int(job[0]),int(job[1]),late)
+        print(late)

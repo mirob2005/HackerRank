5472e7619af899aeab5ab7f21580655740bfb6db
Michael Robertson <mirob2005@gmail.com>
2013-05-04 16:43:58 -0700

    Started triplets, naive approach, can't submit to HackerRank atm

---
+++ Python/Search/triplets.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    N = int(input())
+    
+    numbers = input().split(' ')
+    numbers = [int(x) for x in numbers]
+    
+    visited = [False for x in range(100001)]
+    
+    tuples = set()
+    
+    for index, number in enumerate(numbers):
+        if not visited[number]:
+            first = number
+            while index < N-1:
+                index += 1
+                if number < numbers[index]:
+                    second = numbers[index]
+                    thirdIndex = index
+                    while thirdIndex < N-1:
+                        thirdIndex += 1
+                        if second < numbers[thirdIndex]:
+                            tuples.add((first,second,numbers[thirdIndex]))
+                else:
+                    continue
+
+    print(len(tuples))

e2ca09aac75500627cb15b5551c498f100bbb3c4
Michael Robertson <mirob2005@gmail.com>
2013-05-24 18:15:28 -0700

    Successfully completed string similarity challenge using z-algorithm

--- Python/Strings/README.md
+++ Python/Strings/README.md
@@ -3,12 +3,14 @@
 ## Strings:
 https://www.hackerrank.com/categories/algorithms/strings
 
-- 0/8 Completed Successfully
+- 1/8 Completed Successfully
 
 ### String Similiarity:
 https://www.hackerrank.com/challenges/string-similarity
 
-- 5/10 Correct (5 Time Limit)
-- Submission: https://www.hackerrank.com/submissions/code/650692
+- 10/10 Correct
+- Submission: https://www.hackerrank.com/submissions/code/658196
+- Longest Time: 2.01 seconds where 1<=T<=10 and S <= 100,000
 
-- NOT DONE
+- Done using Z-Algorithm
+- Followed demo from: http://www.utdallas.edu/~besp/demo/John2010/z-algorithm.htm to implement
--- Python/Strings/string_similarity_print_steps.py
+++ Python/Strings/string_similarity_print_steps.py
@@ -3,8 +3,11 @@
 # http://www.utdallas.edu/~besp/demo/John2010/z-algorithm.htm
 # to implement
 
+import time
+
 if __name__ == '__main__':
     N = int(input())
+    s = time.clock()
     
     for line in range(N):
         string = input()
@@ -12,19 +15,25 @@ if __name__ == '__main__':
         length = len(string)
         total = length
 
+        compare = 0
         z = [0]
         l = 0
         r = 0
 
         for k in range(1,length):
+            print('\nEvaluating %s'%k)
             if k > r:
+                print('Compute Explicitly')
                 match=0
                 index = k
                 while index < length:
+                    print('Comparing %s and %s'%(string[index],string[match]))
                     if string[index] == string[match]:
                         match +=1
                         index +=1
+                        compare+=1
                     else:
+                        compare+=1
                         break
                 z.append(match)
                 if match > 0:
@@ -32,20 +41,38 @@ if __name__ == '__main__':
                     l = k
                     r = index-1
             else:
+                print('Previously Discovered Substring')
                 if z[k-l] < (r-k)+1:
+                    print('No Additional comparisons needed')
                     z.append(z[k-l])
                     total+=z[k-l]
                 else:
+                    print('Extended substring')
                     match = r-k
                     index = r
                     while index < length:
+                        print('Comparing %s and %s'%(string[index],string[match]))
                         if string[index] == string[match]:
                             match +=1
                             index +=1
+                            compare+=1
                         else:
+                            compare+=1
                             break
                     z.append(match)
                     total+=match
                     l = k
                     r = index-1
+            print('%s compares so far'%compare)
+        
+            print('z: %s'%str(z))
+            print('l: %s'%l)
+            print('r: %s'%r)
+        print('----------------')
+        print('Total matches %s'%total)
+        print('Total comparsions: %s'%(compare))
         print(total)
+            
+    f = time.clock()
+    totalTime = f-s
+    print(totalTime)
